1decf7a3b89b4b08944b597d0e8c9e00031028c7
Sofya21 <100346168+Sofya21@users.noreply.github.com>
2022-06-17 15:39:43 +0300

    my_game
    15:39

--- scripts.js
+++ scripts.js
@@ -15,23 +15,52 @@ var PLAYER = {
     height: 20,
     score: 0,
     live: 3,
-}   
+}
+
+var METEOR = {
+    x: canvasWidth * 0.5 + 1,
+    y: 100,
+    size: 20,
+    color: "black"
+}
 
 //Настройки
 canvas.width = canvasWidth;
 canvas.height = canvasHeight;
 
 //Функции
-function drawBackground(Color){
-    canvasContext.fillStyle = Color;
+function drawBackground() {
+    canvasContext.fillStyle = "red";
     canvasContext.fillRect(0, 0, canvasWidth, canvasHeight);
 }
 
-function drawPlayer(){
+function drawPlayer() {
     canvasContext.fillStyle = PLAYER.color;
     canvasContext.fillRect(PLAYER.x, PLAYER.y, PLAYER.width, PLAYER.height);
 }
 
-//База
-drawBackground("Red");
-drawPlayer();
+function drawMeteor() {
+    canvasContext.fillStyle = METEOR.color;
+    canvasContext.beginPath();
+    canvasContext.arc(METEOR.x, METEOR.y, METEOR.size, 0, 2 * Math.PI);
+    canvasContext.fill();
+}
+
+function updateMeteor() {
+    METEOR.y += 5;
+}
+
+function drawFrame() {
+    drawBackground();
+    drawPlayer();
+    drawMeteor();
+}
+
+function play() {
+    drawFrame();
+    updateMeteor();
+    requestAnimationFrame(play);
+}
+drawBackground("red");
+play();
+
